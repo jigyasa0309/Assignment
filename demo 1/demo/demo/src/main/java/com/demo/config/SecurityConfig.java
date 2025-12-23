6d4385da6b2e004a8a185cd4e9e1bae5f4392f81
Jigyasa Yadav <120003217+jigyasa0309@users.noreply.github.com>
2025-12-23 10:40:54 +0530

    Update SecurityConfig.java

--- demo 1/demo/demo/src/main/java/com/demo/config/SecurityConfig.java
+++ demo 1/demo/demo/src/main/java/com/demo/config/SecurityConfig.java
@@ -10,13 +10,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/auth/**").permitAll()
+                        auth
+                                .requestMatchers("/auth/**").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtFilter,
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
