c35293f3df0510eccfea1a2f596d2c8fde9e2a26
Jigyasa Yadav <120003217+jigyasa0309@users.noreply.github.com>
2025-12-23 11:06:06 +0530

    Update TaskResponse.java

--- demo 1/demo/demo/src/main/java/com/demo/dto/TaskResponse.java
+++ demo 1/demo/demo/src/main/java/com/demo/dto/TaskResponse.java
@@ -1,14 +1,19 @@
 package com.demo.dto;
 
 import com.demo.entity.TaskStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskResponse {
 
     private Long id;
