a581a5f5d5b7bd4bc459e3bd7adb94f397de739e
Jigyasa Yadav <120003217+jigyasa0309@users.noreply.github.com>
2025-12-23 11:06:19 +0530

    Update ProjectResponse.java

--- demo 1/demo/demo/src/main/java/com/demo/dto/ProjectResponse.java
+++ demo 1/demo/demo/src/main/java/com/demo/dto/ProjectResponse.java
@@ -4,11 +4,18 @@ package com.demo.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectResponse {
 
     private Long id;
